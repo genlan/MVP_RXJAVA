ee14ec579ba22de7437ba3f2f01ae1c433c9f1f2
langengen <874938631@qq.com>
2016-04-19 11:30:36 +0800

    测试

---
+++ app/src/main/java/com/example/bean/User3.java
@@ -0,0 +1,29 @@
+package com.example.bean;
+
+/**
+ * Created by Administrator on 2015/12/22.
+ */
+public class User3 {
+
+    private  String username;
+
+    private  String pwd;
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+}
